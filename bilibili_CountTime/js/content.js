31abedf764c5647cc6f4d35382990faf9d2949f6
sEGuoer <709074535@qq.com>
2023-08-08 14:28:03 +0800

    针对B站分P和合集不同情况下当前时间和总时长的显示

--- bilibili_CountTime/js/content.js
+++ bilibili_CountTime/js/content.js
@@ -1,90 +1,20 @@
-setInterval(function () {
-        var allTime = document.querySelectorAll(".video-episode-card__info-duration")
-        let time = 0
-        let needtime = 0
-        let selectWhichIsLooking = 0
-        allTime.forEach(function (element) {
-            let timeGroup = element.textContent.split(':')
-            if (selectWhichIsLooking === 1) {
-                if (timeGroup.length === 3) {
-                    let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
-                    time = time + elementTime
-                    needtime = needtime + elementTime
-
-                } else if (timeGroup.length === 2) {
-                    let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
-                    time = time + elementTime
-                    needtime = needtime + elementTime
-                } else if (timeGroup.length === 1) {
-                    let elementTime = parseInt(timeGroup[0])
-                    time = time + elementTime
-                    needtime = needtime + elementTime
-
-                }
-            } else if (selectWhichIsLooking === 0) {
-                var a = element.parentElement.querySelector(".video-episode-card__info-title .cur-play-icon")
-                console.log(a.getAttribute("style"))
-                if (a.getAttribute("style") === "display:none;") {
-                    if (timeGroup.length === 3) {
-                        let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
-                        time = time + elementTime
-
-                    } else if (timeGroup.length === 2) {
-                        let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
-
-                        time = time + elementTime
-                    } else if (timeGroup.length === 1) {
-                        let elementTime = parseInt(timeGroup[0])
-                        time = time + elementTime
-                    }
-                } else {
-                    var b = document.querySelector(".bpx-player-ctrl-time-label .bpx-player-ctrl-time-current")
-                    let currentTimeGroup = b.textContent.split(':')
-                    selectWhichIsLooking = 1
-                    if (timeGroup.length === 3) {
-                        let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
-                        let deleteTime = deleteCurrentTime(currentTimeGroup)
-                        time = time + elementTime
-                        needtime = needtime + elementTime - deleteTime
-
-                    } else if (timeGroup.length === 2) {
-                        let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
-                        let deleteTime = deleteCurrentTime(currentTimeGroup)
-                        time = time + elementTime
-                        needtime = needtime + elementTime - deleteTime
-                    } else if (timeGroup.length === 1) {
-                        let elementTime = parseInt(timeGroup[0])
-                        let deleteTime = deleteCurrentTime(currentTimeGroup)
-                        time = time + elementTime
-                        needtime = needtime + elementTime - deleteTime
-                    }
-                }
-            }
-        })
-        console.log(time)
-        let hour1 = parseInt(time / 3600)
-        console.log(hour1)
-
-        let minute1 = parseInt(time % 3600 / 60)
-        console.log(minute1)
-
-        let second1 = time % 3600 % 60
-        console.log(second1)
-        let addContent1 = document.getElementById("viewbox_report")
-        addUseTime("看完合集用时" + hour1 + ":" + minute1 + ":" + second1, "sumTimeWillBeUse", addContent1)
-        console.log(needtime)
-        let hour2 = parseInt(needtime / 3600)
-        console.log(hour2)
-
-        let minute2 = parseInt(needtime % 3600 / 60)
-        console.log(minute2)
-
-        let second2 = needtime % 3600 % 60
-        console.log(second2)
-        let addContent2 = document.querySelector(".first-line-left .cur-page")
-        addUseTime("看完剩余合集用时" + hour2 + ":" + minute2 + ":" + second2, "remainTime", addContent2)
+if (!document.querySelector(".video-episode-card__info-duration")) {
+    if (!document.querySelector(".router-link-active .clickitem .duration")) {
+    } else {
+        heji(".router-link-active .duration", ".link-content img", ".bpx-player-ctrl-time-label .bpx-player-ctrl-time-current", "viewbox_report", "#multi_page .head-con .head-left")
     }
-    , 2000)
+} else {
+    heji(".video-episode-card__info-duration", ".video-episode-card__info-title .cur-play-icon", ".bpx-player-ctrl-time-label .bpx-player-ctrl-time-current", "viewbox_report", ".first-line-left .cur-page")
+}
+
+
+function heji(a, b, c, d, e) {
+    setInterval(function () {
+        hejiMethod(a, b, c, d, e)
+    }, 2000)
+}
+
+hejiMethod(".video-episode-card__info-title .cur-play-icon", ".video-episode-card__info-title .cur-play-icon", ".bpx-player-ctrl-time-label .bpx-player-ctrl-time-current", "viewbox_report", ".first-line-left .cur-page")
 
 function deleteCurrentTime(arg) {
     let deleteCurrentTime = 0
@@ -110,4 +40,91 @@ function addUseTime(String, Timename, addContent) {
     } else {
         document.querySelector("div." + Timename).innerText = String;
     }
+}
+
+function hejiMethod(act, bond, cat, dog, email) {
+    var allTime = document.querySelectorAll(act)
+    let time = 0
+    let needtime = 0
+    let selectWhichIsLooking = 0
+    allTime.forEach(function (element) {
+        let timeGroup = element.textContent.split(':')
+        if (selectWhichIsLooking === 1) {
+            if (timeGroup.length === 3) {
+                let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
+                time = time + elementTime
+                needtime = needtime + elementTime
+
+            } else if (timeGroup.length === 2) {
+                let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
+                time = time + elementTime
+                needtime = needtime + elementTime
+            } else if (timeGroup.length === 1) {
+                let elementTime = parseInt(timeGroup[0])
+                time = time + elementTime
+                needtime = needtime + elementTime
+
+            }
+        } else if (selectWhichIsLooking === 0) {
+            var a = element.parentElement.querySelector(bond)
+            console.log(a.getAttribute("style"))
+            if (a.getAttribute("style") === "display:none;" || a.getAttribute("style") === "display: none;") {
+                if (timeGroup.length === 3) {
+                    let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
+                    time = time + elementTime
+
+                } else if (timeGroup.length === 2) {
+                    let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
+
+                    time = time + elementTime
+                } else if (timeGroup.length === 1) {
+                    let elementTime = parseInt(timeGroup[0])
+                    time = time + elementTime
+                }
+            } else {
+                var b = document.querySelector(cat)
+                let currentTimeGroup = b.textContent.split(':')
+                selectWhichIsLooking = 1
+                if (timeGroup.length === 3) {
+                    let elementTime = parseInt(timeGroup[0]) * 3600 + parseInt(timeGroup[1]) * 60 + parseInt(timeGroup[2])
+                    let deleteTime = deleteCurrentTime(currentTimeGroup)
+                    time = time + elementTime
+                    needtime = needtime + elementTime - deleteTime
+
+                } else if (timeGroup.length === 2) {
+                    let elementTime = parseInt(timeGroup[0]) * 60 + parseInt(timeGroup[1])
+                    let deleteTime = deleteCurrentTime(currentTimeGroup)
+                    time = time + elementTime
+                    needtime = needtime + elementTime - deleteTime
+                } else if (timeGroup.length === 1) {
+                    let elementTime = parseInt(timeGroup[0])
+                    let deleteTime = deleteCurrentTime(currentTimeGroup)
+                    time = time + elementTime
+                    needtime = needtime + elementTime - deleteTime
+                }
+            }
+        }
+    })
+    console.log(time)
+    let hour1 = parseInt(time / 3600)
+    console.log(hour1)
+
+    let minute1 = parseInt(time % 3600 / 60)
+    console.log(minute1)
+
+    let second1 = time % 3600 % 60
+    console.log(second1)
+    let addContent1 = document.getElementById(dog)
+    addUseTime("看完合集用时" + hour1 + ":" + minute1 + ":" + second1, "sumTimeWillBeUse", addContent1)
+    console.log(needtime)
+    let hour2 = parseInt(needtime / 3600)
+    console.log(hour2)
+
+    let minute2 = parseInt(needtime % 3600 / 60)
+    console.log(minute2)
+
+    let second2 = needtime % 3600 % 60
+    console.log(second2)
+    let addContent2 = document.querySelector(email)
+    addUseTime("看完剩余合集用时" + hour2 + ":" + minute2 + ":" + second2, "remainTime", addContent2)
 }
